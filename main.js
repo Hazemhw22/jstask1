02732ea2ca5f90ec91991d798f74426a3a3ac05e
unknown <hazem.denezi@gmail.com>
2024-02-08 18:14:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var age=23;
+var username="Hazem";
+var lastname="Hawamedah";
+var rating=9.5;
+var admin = true;
+
+console.log(typeof age);
+console.log(typeof username);
+console.log(typeof rating);
+console.log(typeof lastname);
+console.log(typeof admin);
